8389a4ffbc5427e250827743f8abb04c331b29ab
nexus27k <joshuakaaya216@gmail.com>
2023-02-17 17:57:50 -0800

    updated 100-print_comb3.c file error corrected

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-
 /**
- *main - Entry point, print 00 to 99 using putchar
- *Return: Always 0 (Success)
- */
-
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	int tens;
-	int ones;
+	int digit1, digit2;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar(tens);
-			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
+
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
