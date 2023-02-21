0a3907e62e9acc20868a3d88cb3ecd9c72b35f81
nexus27k <joshuakaaya216@gmail.com>
2023-02-21 11:48:12 -0800

    Created function _putchar and used it to display some texts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
-int main()
+#include "main.h"
+/**
+ * This program will print out (_putcahr) and newline
+ * Return: 0 'void'
+ */
+int main(void)
 {
-	printf("_putchar \n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+ * File Name: main.h
+ * Description: This is the header file containing function declarations
+ */
+
+int _putchar(char);
+
