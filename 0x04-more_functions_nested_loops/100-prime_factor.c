af6fc89ccd05f42a7097f8fe584c5411dc54206a
JoshuaWilfred <joshuakaya216@gmail.com>
2024-03-24 04:21:37 -0700

    Review 2.1 Functions in C

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * main - find and print the largest prim efactor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long i, largest;
+	long n;
+
+	n = 612852475143;
+	largest = 1;
+
+	for (i = 3; i < n / 2; i = i + 2)
+	{
+		if (n % i == 0)
+		{
+			largest = i;
+			n = n / i;
+		}
+	}
+	if (largest == 1)
+		printf("%ld\n", n);
+	else
+		printf("%ld\n", n);
+
+	return (0);
+} 
