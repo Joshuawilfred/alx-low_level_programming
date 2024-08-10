5635374e942561f6c89de9086ff675b03c8d009d
Joshua Wilfred <joshuakaaya216@gmail.com>
2024-08-10 05:39:56 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int find_middle(int *array, int low, int high, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
