c5c0a759529cd8a516bbfdf1d937069ae83b137e
Joshua Wilfred <joshuakaaya216@gmail.com>
2024-08-10 05:52:35 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,9 @@
 #include <stddef.h> 
 #include <stdlib.h>
 
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int find_middle(int *array, int low, int high, size_t size, int value);
